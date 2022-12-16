3ed8b03a8d1b199c2be9e0a7c1a5809b47cdddfc
beccis <rebecca.lindberg@outlook.com>
2022-12-16 17:32:23 +0100

    remove comment

--- api/models.py
+++ api/models.py
@@ -10,7 +10,6 @@ class Message(models.Model):
     subject = models.CharField(max_length=100)
     message = models.TextField()
     date_sent = models.DateTimeField(auto_now_add=True)
-    #Cascade gör att när en user deletas, så deletas alla kopplade messages till det också
     recipient = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
